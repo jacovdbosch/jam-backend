29bb8127fd76c85bafc428771b8c23cfb362697e
Jaco van den Bosch <jacovdboschjr@gmail.com>
2020-03-12 11:18:43 +0100

    add dummy data so we can query

--- src/index.js
+++ src/index.js
@@ -1,45 +1,16 @@
-const { gql } = require('apollo-server');
+const {ApolloServer} = require('apollo-server');
+const typeDefs = require('./schema');
+const quiz = require('./database');
 
-const typeDefs = gql`    
-    type Player {
-        score: Int!
-        answers: [Answer]!
-    }
-    
-    type Answer {
-        id: ID!
-        value: String!
-    }
-    
-    type Question {
-        id: ID!
-        value: String!
-        label: String!
-    }
-    
-    type Quiz {
-        id: ID!
-        players: [Player]!,
-        questions: [Question]!,
-    }
-    
-    type Query {
-        getQuiz(id: ID!): Quiz
-    }
-    
-    type Mutation {
-        startQuiz(quizId: ID!): Quiz
-        joinQuiz(quizId: ID!, name: String): Quiz
-        startQuestion(quizId: ID!): Boolean
-        answerQuestion(quizId: ID!, questionId: ID!, answer: String!): Answer
-    }
-    
-    type Subscription {
-        quizStarted(quizId: ID!): Quiz
-        quizEnded(quizId: ID!): Quiz
-        questionHasStarted(quizId: ID!): Question
-        questionHasEnded(questionId: ID!): Question
-        playerJoinedQuiz(quizId: ID!): Player
-        playerAnsweredQuestion(questionId: ID!): Answer
-    }
-`;
+
+const server = new ApolloServer({
+    typeDefs, resolvers: {
+        Query: {
+            getQuiz: () => quiz
+        }
+    },
+});
+
+server.listen().then((v) => {
+    console.log(`listening on port ${v.port}`);
+});
